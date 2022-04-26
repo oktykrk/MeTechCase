41a41fa3983cd80591ec4a7b36c0477ec499ac08
Oktay Kirik <v-oktaykirik@microsoft.com>
2022-04-27 00:33:03 +0300

    test

---
+++ MeTechCase/Controllers/OrdersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace MeTechCase.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly ILogger<OrdersController> _logger;
+
+        public OrdersController(ILogger<OrdersController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Test()
+        {
+            return "HELLO TEST";
+        }
+    }
+}
